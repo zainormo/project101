5026afdc00012f892a617921bdb73f72c015a45f
Laptop House <Laptop House@AHMAD>
2026-05-08 23:59:07 +0500

    Add project files.

---
+++ WebApplication1/Controllers/AccountControllers.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: /Account/SignIn
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        // POST: /Account/SignIn
+        [HttpPost]
+        public IActionResult SignIn(string email, string password)
+        {
+            // login logic will go here later
+            return RedirectToAction("Index", "Home");
+        }
+
+        // GET: /Account/Register
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: /Account/Register
+        [HttpPost]
+        public IActionResult Register(string FullName, string CNIC, string RegNumber, string Address, string SecurityPaid)
+        {
+            // register logic will go here later
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
